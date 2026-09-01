d1d077ee5192b7417923a08411a12e6b5cdd1d5e
Muskan Singh <muskansingh800415@gmail.com>
2026-09-01 07:48:31 +0530

    first day code

---
+++ day1.py
@@ -0,0 +1,3 @@
+import numpy as np
+A = np.array([[2,1],[1,2]])
+print(np.linalg.eig(A))
---
+++ day2.py
@@ -0,0 +1,8 @@
+import numpy as np
+#1.List ko array banana
+mark = np.array([80, 90, 75])
+
+#2.Average 1 line me 
+print(mark.mean())
+A = np.array([[2,1],[1,2]])
+print(np.linalg.eig(A))
